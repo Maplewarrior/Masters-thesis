d4c650a41baba7413a370779cec1c311161f67bc
August Tollerup <augusttollerup@Augusts-MacBook-Pro.local>
2025-02-04 12:17:49 +0100

    Changes to NN to make it 'work' and more sisa

--- src/modelling/sisa.py
+++ src/modelling/sisa.py
@@ -4,11 +4,12 @@ from neural_network import NeuralNetwork
 from pprint import pprint
 import numpy as np
 import pdb
+import torch
 
 class Shard(BaseModel):
     shard_indices: list[int]
     slices: list[list[int]]
-
+    
 class Slice(BaseModel):
     slice_id: str
     slice_indices: list[int]
@@ -18,8 +19,15 @@ class ShardsDict(BaseModel):
 
 class SISA:
     def __init__(self, dataset: SyntheticDataset, 
-                 n_shards: int = 10, n_slices: int = 10):
+                 n_shards: int = 10, n_slices: int = 10,
+                 n_features: int = 2, n_classes: int = 2,
+                 n_epochs: int = 10):
         self.dataset = dataset
+
+        self.shard_models = {} # {shard_id: {slice_id: model}}
+
+        self.n_classes = n_classes
+        self.n_features = n_features
 
         self.shards_dict = ShardsDict(shards={})
 
@@ -27,6 +35,8 @@ class SISA:
         self.n_slices = n_slices
         self.shard_size = len(self.dataset.X) // self.n_shards
         self.slice_size = self.shard_size // self.n_slices
+
+        self.n_epochs = n_epochs
 
         """
         We could save all the shards in files, but we want to save them in memory for now.
@@ -48,6 +58,8 @@ class SISA:
             shard_indices = np.random.choice(len(self.dataset.X), self.shard_size, replace=False)
             self.shards_dict.shards[f"shard_{i}"] = Shard(shard_indices=shard_indices, slices=[])
 
+        return self.shards_dict
+
     def slice_shards(self):
         """
         We slice the shards into n_slices slices.
@@ -58,6 +70,8 @@ class SISA:
             for i in range(self.n_slices):
                 slice_indices = np.random.choice(shard_indices, self.slice_size, replace=False)
                 shard.slices.append(slice_indices)
+
+        return self.shards_dict
 
     def process_data(self):
         """
@@ -75,21 +89,78 @@ class SISA:
 
         return self.shards_dict
     
-    def train_model_on_shard(self, model: NeuralNetwork, shard_id: int):
-        shard = self.shards_dict.shards[f"shard_{shard_id}"] # This might be unclear to some
-        shard_indices = shard.shard_indices
-        shard_data = self.dataset.X[shard_indices]
-        shard_labels = self.dataset.y[shard_indices]
+    def train_model_on_shard(self, shard_id: int):
+        """
+        We train the model on a distinct shard.
+        This function loops over all slices for a shard and trains a model on each slice.
 
-        model.fit(shard_data, shard_labels)
+        We also implement a dynamic epoch size based on SISA (Eq. 1):
+        # e'D = sum(e_i * (iD/R), i=1 to R)
+        #     = e 
+        #     = (2R/(R+1))e'
+
+        Where R is the number of slices we train on and e' is the number of epochs to train without slicing.
+        
+        Args:
+            shard_id (int): The id of the shard to train on.
+
+        Returns:
+            model (NeuralNetwork): The trained model.
+        """
+
+        model = NeuralNetwork(self.n_features, self.n_classes)
+
+        shard = self.shards_dict.shards[f"shard_{shard_id}"] # This might be unclear to some
+        shard_slices = shard.slices
+        shard_models = {}
+
+        
+        
+        # Here we incrementally increase the amount of slices we train on.
+        # M_k,1 uses 1 slice, M_k,2 uses 1:2 slices, ..., M_k,k uses 1:k slices.
+        for slice_id, _ in enumerate(shard_slices):
+            slice_indices = np.array(shard_slices[:slice_id+1]).flatten()
+            amount_of_slices = slice_id + 1
+            n_epochs = int((2 * amount_of_slices / (amount_of_slices + 1)) * self.n_epochs)
+            
+            print(f"\nTraining model on slices 1:{amount_of_slices} of shard {shard_id}")
+            print(f"Dynamic epochs: {n_epochs}")
+            print(f"Slice indices: {slice_indices}")
+            
+            slice_data = self.dataset.X[slice_indices]
+            slice_labels = self.dataset.y[slice_indices]
+
+            # Convert labels to one-hot encoding
+            slice_labels = torch.tensor(slice_labels, dtype=torch.long)
+
+            # Convert shard_data and shard_labels to tensors
+            slice_data = torch.tensor(slice_data, dtype=torch.float32)
+
+            model.fit(slice_data, slice_labels, n_epochs=n_epochs)
+
+            shard_models[f"slice_{slice_id}"] = model
+
+        self.shard_models[f"shard_{shard_id}"] = shard_models
+
+        print(f"\nFinished training models on shard {shard_id}")
+        return self.shard_models
+
+    def train_all_models(self):
+        for shard_id in range(self.n_shards):
+            self.train_model_on_shard(shard_id)
+
+        pdb.set_trace()
+
+        return self.shard_models
 
 if __name__ == "__main__":
+    n_features = 2
+    n_classes = 2
     data_generator = DataGenerator(random_state=42)
-    data_generator.generate_data(n_samples=1000, n_features=2, 
-                             n_informative=2, n_redundant=0)
+    data_generator.generate_data(n_samples=1000, n_features=n_features, 
+                             n_informative=2, n_redundant=0, n_classes=n_classes)
     
-    sisa = SISA(data_generator)
+    sisa = SISA(data_generator, n_classes=n_classes, n_features=n_features)
     shards_dict = sisa.process_data()
 
-    model = NeuralNetwork(M=2, n_classes=2)
-    sisa.train_model_on_shard(model, 0)
+    sisa.train_all_models()
