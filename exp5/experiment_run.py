5cd01b370b8ead26d47c1dfa88085acc9377d773
Andreas Fiehn <andreas@lyhnefiehn.dk>
2025-06-11 09:52:46 +0200

    FEAT: Added repair steps to experiment run

--- exp5/experiment_run.py
+++ exp5/experiment_run.py
@@ -154,7 +154,7 @@ def main(cfg):
         results_root_dir = os.path.join(absolute_root_path, 'results', f'teacher_ascend_{hyperparams_postfix}')
         weights_root_dir = os.path.join(absolute_root_path, 'weights', f'teacher_ascend_{hyperparams_postfix}')
     elif cfg.unlearn.method == 'scrub':
-        hyperparams_postfix = f"{cfg.unlearn.scrub.alpha}alpha_{cfg.unlearn.scrub.gamma}gamma_{cfg.unlearn.scrub.n_rounds}rounds"
+        hyperparams_postfix = f"{cfg.unlearn.scrub.alpha}alpha_{cfg.unlearn.scrub.gamma}gamma_{cfg.unlearn.scrub.n_rounds}rounds_{cfg.unlearn.scrub.n_repair_rounds}repair_rounds"
         results_root_dir = os.path.join(absolute_root_path, 'results', f'scrub_{hyperparams_postfix}')
         weights_root_dir = os.path.join(absolute_root_path, 'weights', f'scrub_{hyperparams_postfix}')
     else:
@@ -332,14 +332,14 @@ def main(cfg):
         from src.unlearners.scrub import ScrubR
         unlearning_evaluator = UnlearningEvaluator(device=DEVICE)
         js_div_func = unlearning_evaluator.JS_divergence
-        hyperparams = {'alpha': cfg.unlearn.scrub.alpha, 'gamma': cfg.unlearn.scrub.gamma, 'n_epochs': cfg.unlearn.scrub.n_rounds}
+        hyperparams = {'alpha': cfg.unlearn.scrub.alpha, 'gamma': cfg.unlearn.scrub.gamma, 'n_epochs': cfg.unlearn.scrub.n_rounds, 'n_repair_rounds': cfg.unlearn.scrub.n_repair_rounds}
 
         print("Initializing SCRUB")
         scrub_model = copy.deepcopy(original_model)
         scrub = ScrubR(scrub_model, original_model, alpha=hyperparams['alpha'], gamma=hyperparams['gamma'], device=DEVICE, MIA=mia_model, js_div_func=js_div_func, retrain_model=retrained_model)
 
 
-        scrub_metrics = scrub(dataloader_retain, dataloader_forget, dataloader_val, n_rounds=hyperparams['n_epochs'], verbose=True)
+        scrub_metrics = scrub(dataloader_retain, dataloader_forget, dataloader_val, n_rounds=hyperparams['n_epochs'], n_repair_rounds=hyperparams['n_repair_rounds'], verbose=True)
 
         with open(f'{results_dir}/scrub_metrics.json', 'w') as f:
             json.dump(scrub_metrics, f)
