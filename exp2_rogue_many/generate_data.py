5c11737120c14dbc6b18bca4df1fa3c9944c4c79
Andreas Fiehn <andreas@lyhnefiehn.dk>
2025-06-19 15:54:39 +0200

    REFAC: Data folder path construction to use current directory

--- exp2_rogue_many/generate_data.py
+++ exp2_rogue_many/generate_data.py
@@ -151,7 +151,8 @@ if __name__ == "__main__":
     sizes = np.array([20, 20, 20])
     validation_sizes = np.array([10, 10, 10])
 
-    data_folder = 'exp2_rogue_many/data'
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_folder = os.path.join(current_dir, 'data')
     data_plots_folder = os.path.join(data_folder, 'plots')
     os.makedirs(data_plots_folder, exist_ok=True)
 
