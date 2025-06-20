b2e480c0a2d5a8899d0a40a4acd77a9b6e77b79e
Andreas Fiehn <andreas@lyhnefiehn.dk>
2025-06-20 10:55:49 +0200

    FEAT: Updated folders to use model names and hyperparams

--- exp1_decision_boundary/experiment_run.py
+++ exp1_decision_boundary/experiment_run.py
@@ -19,9 +19,8 @@ def main(cfg):
     validation_path = os.path.join("/",*dataset_path.split("/")[1:-1], "validation_data.npz")
 
     rogue = dataset_path.split("/")[-2] # rogue_many or rogue_one
-    results_dir = os.path.join(os.path.dirname(__file__), "results", rogue)
-    os.makedirs(results_dir, exist_ok=True)
-
+    # replace _ with nothing
+    rogue = rogue.replace("_", "")
 
     # ============== Load data ============== 
     dataloader_train, dataloader_val, dataloader_retain, dataloader_forget, forget_idxs = get_data(dataset_path, validation_path, cfg.data.batch_size, cfg.data.n_classes)
@@ -31,6 +30,8 @@ def main(cfg):
 
     # No logger, this could be changed to a wandb logger if needed
     logger = None 
+
+    decision_boundary_filename = f"{dataset_number}{rogue}_{cfg.unlearn.method}_decision_boundary" 
 
     if cfg.unlearn.method == "retrain":
 
@@ -60,14 +61,15 @@ def main(cfg):
                                        disable_tqdm=cfg.trainer.disable_tqdm, 
                                        do_early_stopping=cfg.trainer.do_early_stopping)()
 
+        results_dir = os.path.join(os.path.dirname(__file__), "results", cfg.unlearn.method)
+        os.makedirs(results_dir, exist_ok=True)
 
         plot_title = f"Retrained"
-        decision_boundary_filename = f"{dataset_number}_{cfg.unlearn.method}_decision_boundary" 
         decision_boundary_plot(model, original_model, dataloader_retain, dataloader_train, dataloader_forget.dataset.X, results_dir, plot_title=plot_title, filename=decision_boundary_filename)
 
     else:
         if cfg.unlearn.method not in ['amnesiac', 'sisa']:
-            unlearned_model = NeuralNet(M=X.shape[1], n_classes=cfg.data.n_classes, seed=cfg.model.seed)
+            unlearned_model = NeuralNet(M=dataloader_train.dataset.X.shape[1], n_classes=cfg.data.n_classes, seed=cfg.model.seed)
             NeuralNetworkTrainer(model=unlearned_model, 
                                  train_dataloader=dataloader_train, 
                                  val_dataloader=dataloader_val, 
@@ -83,31 +85,45 @@ def main(cfg):
         if cfg.unlearn.method == "scrubr":
 
             from src.unlearners.scrub import ScrubR
+
+            alpha = 1
+            gamma = 1
+            n_rounds = 4
+            n_repair_rounds = 6
+
             ScrubR(model=unlearned_model, 
                    original_model=original_model,
-                   alpha=1,
-                   gamma=1,
+                   alpha=alpha,
+                   gamma=gamma,
                    device=DEVICE)(retain_dataloader=dataloader_retain, 
                                   forget_dataloader=dataloader_forget, 
                                   val_dataloader=dataloader_val, 
-                                  n_rounds=4, # min + max rounds
-                                  n_repair_rounds=6 # min only rounds 
+                                  n_rounds=n_rounds, # min + max rounds
+                                  n_repair_rounds=n_repair_rounds # min only rounds 
                                   )
+            
+            results_dir = os.path.join(os.path.dirname(__file__), "results", f"{cfg.unlearn.method}_alpha{alpha}_gamma{gamma}_n_rounds{n_rounds}_n_repair_rounds{n_repair_rounds}")
+            os.makedirs(results_dir, exist_ok=True)
             plot_title = f"SCRUB+R"
-            decision_boundary_filename = f"{dataset_number}_{cfg.unlearn.method}_decision_boundary"
             decision_boundary_plot(unlearned_model, original_model, dataloader_retain, dataloader_train, dataloader_forget.dataset.X, results_dir, plot_title=plot_title, filename=decision_boundary_filename)
         
         elif cfg.unlearn.method == "ssd":
             from src.unlearners.selective_synaptic_dampening import SelectiveSynapticDampening
 
+            alpha = 1
+            _lambda = 1
+
             SelectiveSynapticDampening(unlearned_model, 
-                                       alpha=1, 
-                                       _lambda=1,
+                                       alpha=alpha, 
+                                       _lambda=_lambda,
                                        device=DEVICE)(full_dataloader=dataloader_train, 
                                                       forget_dataloader=dataloader_forget)
             
+            # add hyperparameters to results folder name
+            results_dir = os.path.join(os.path.dirname(__file__), "results", f"{cfg.unlearn.method}_alpha{alpha}_lambda{_lambda}")
+            os.makedirs(results_dir, exist_ok=True)
+
             plot_title = f"SSD"
-            decision_boundary_filename = f"{dataset_number}_{cfg.unlearn.method}_decision_boundary"
             decision_boundary_plot(unlearned_model, original_model, dataloader_retain, dataloader_train, dataloader_forget.dataset.X, results_dir, plot_title=plot_title, filename=decision_boundary_filename)
         
         elif cfg.unlearn.method == 'sae':
@@ -116,24 +132,30 @@ def main(cfg):
             from src.models.neural_net_with_sae import NeuralNetWithSAE
             from src.trainers.sae_trainer import SAETrainer
             from src.unlearners.sae_unlearner import SAEUnlearner
+
+            alpha = 0.9
+            layer_num = 6
+
             # instantiate & train neural network
-            neural_net = NeuralNetRS(X.shape[1], cfg.data.n_classes)
+            neural_net = NeuralNetRS(dataloader_train.dataset.X.shape[1], cfg.data.n_classes)
             nn_trainer = NeuralNetworkTrainer(neural_net, dataloader_train, dataloader_val)
             nn_trainer.train()
             original_model = copy.deepcopy(neural_net)
             
             # instantiate & train SAE
             sae = SAE(d=8, m=32, _lambda=1.).to(DEVICE)
-            unlearned_model = NeuralNetWithSAE(neural_net, sae, layer_num=6)
+            unlearned_model = NeuralNetWithSAE(neural_net, sae, layer_num=layer_num)
             sae_trainer = SAETrainer(unlearned_model, dataloader_train, dataloader_val, logger=None, device=DEVICE)            
             sae_trainer.train()
 
             # unlearn with feature dampening
-            sae_unlearner = SAEUnlearner(unlearned_model, alpha=0.9, device=DEVICE)
+            sae_unlearner = SAEUnlearner(unlearned_model, alpha=alpha, device=DEVICE)
             sae_unlearner(dataloader_retain, dataloader_forget)
 
+            results_dir = os.path.join(os.path.dirname(__file__), "results", f"{cfg.unlearn.method}_alpha{alpha}_layer_num{layer_num}")
+            os.makedirs(results_dir, exist_ok=True)
+
             plot_title = f"SAE"
-            decision_boundary_filename = f"{dataset_number}_{cfg.unlearn.method}_decision_boundary"
             decision_boundary_plot(unlearned_model, original_model, dataloader_retain, dataloader_train, dataloader_forget.dataset.X, results_dir, plot_title=plot_title, filename=decision_boundary_filename)
       
         elif cfg.unlearn.method == "amnesiac":
@@ -141,7 +163,7 @@ def main(cfg):
             from src.models.amnesiac_model import AmnesiacModel
             from src.trainers.amnesiac_trainer import AmnesiacTrainer
             
-            unlearned_model = NeuralNet(M=X.shape[1], n_classes=cfg.data.n_classes, seed=cfg.model.seed)
+            unlearned_model = NeuralNet(M=dataloader_train.dataset.X.shape[1], n_classes=cfg.data.n_classes, seed=cfg.model.seed)
             # Wrap the unlearned model in an AmnesiacModel
             unlearned_model = AmnesiacModel(unlearned_model)
 
@@ -164,6 +186,9 @@ def main(cfg):
             AmnesiacUnlearner(model=unlearned_model, 
                               unlearn_parameters=cfg.unlearn)(indices_to_forget=forget_idxs)
 
+            results_dir = os.path.join(os.path.dirname(__file__), "results", f"{cfg.unlearn.method}")
+            os.makedirs(results_dir, exist_ok=True)
+
             plot_title = f"Amnesiac"
             decision_boundary_filename = f"{dataset_number}_{cfg.unlearn.method}_decision_boundary"
             decision_boundary_plot(unlearned_model, original_model, dataloader_retain, dataloader_train, dataloader_forget.dataset.X, results_dir, plot_title=plot_title, filename=decision_boundary_filename)
@@ -176,10 +201,11 @@ def main(cfg):
             
             weights_dir = os.path.join(os.path.dirname(__file__), "weights")
             sisa_model_fn = NeuralNet
-            sisa_model_parameters = {'M': X.shape[1],
+            sisa_model_parameters = {'M': dataloader_train.dataset.X.shape[1],
                                      'n_classes': cfg.data.n_classes,
                                      'seed': cfg.model.seed}
             sisa_optimzier_parameters = {'lr': cfg.trainer.lr}
+
 
             sisa = SISA(dataloader_train,
                         dataloader_val,
@@ -213,6 +239,9 @@ def main(cfg):
             # get unlearned SISA model
             sisa_unlearner = SISAUnlearner(sisa)
             sisa_unlearner(forget_indices=forget_idxs)
+
+            results_dir = os.path.join(os.path.dirname(__file__), "results", f"{cfg.unlearn.method}_n_shards{cfg.sisa.n_shards}_n_slices{cfg.sisa.n_slices}")
+            os.makedirs(results_dir, exist_ok=True)
 
             plot_title = f"SISA"
             decision_boundary_filename = f"{dataset_number}_{cfg.unlearn.method}_decision_boundary"
