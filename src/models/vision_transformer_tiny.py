f0c35bfb3725e668cf185d97c8c2fef87c358d90
Maplewarrior <michaelharborg@gmail.com>
2025-06-21 14:50:22 +0200

    fix ViT forward pass for SAE

--- src/models/vision_transformer_tiny.py
+++ src/models/vision_transformer_tiny.py
@@ -1,10 +1,11 @@
+import torch
 import torch.nn as nn
 from transformers import ViTForImageClassification, ViTModel
 from src.models.base_model import BaseModel
 import pdb
 
 def build_backbone():
-    model = ViTModel.from_pretrained("WinKawaks/vit-tiny-patch16-224")
+    model = ViTModel.from_pretrained("WinKawaks/vit-tiny-patch16-224", add_pooling_layer=False)
     return model
 
 class ViT(BaseModel):
@@ -34,13 +35,18 @@ class ViT(BaseModel):
         self.tau = tau
         self.pooling_type = pooling_type
         self.backbone = build_backbone()
-        # self.backbone = ViTForImageClassification.from_pretrained("google/vit-base-patch16-224") #ViTForImageClassification.from_pretrained("WinKawaks/vit-tiny-patch16-224")
         self.linear_probe = nn.Linear(d_hidden, n_classes)
         self.dropout = nn.Dropout(self.dropout_prob)
         self.softmax = nn.Softmax(dim=-1)        
     
-    def forward(self, x):
+    def forward(self, x, start_idx = 0, stop_idx=None):
         # process using pretrained vit
+        if start_idx == 0 and stop_idx == None:
+            return self._forward_full(x)
+        else:
+            return self._forward_selective(x, start_idx, stop_idx)
+    
+    def _forward_full(self, x):
         x = self.backbone(x).last_hidden_state
         x = self.dropout(x)
         if self.pooling_type == 'max':
@@ -53,5 +59,31 @@ class ViT(BaseModel):
             x = x[:, 0]
         
         logits = self.linear_probe(x)
-        return {'logits': logits, 'probabilities': self.softmax(logits)}
-                 
+        return {'logits': logits, 'probabilities': self.softmax(logits), 'predictions': torch.argmax(logits, dim=-1)}
+    
+    def _forward_selective(self, x, start_idx, stop_idx):
+        if start_idx == 0:
+            x = self.backbone.embeddings(x)
+        if stop_idx == None:
+            stop_idx = len(self.backbone.encoder.layer)
+        for i in range(start_idx, min(stop_idx, len(self.backbone.encoder.layer))):
+            x = self.backbone.encoder.layer[i](x)[0]
+        
+        # return intermediate state
+        if stop_idx != len(self.backbone.encoder.layer):
+            return {'logits': x}
+        
+        # complete forward pass
+        x = self.backbone.layernorm(x)
+        x = self.dropout(x)
+        if self.pooling_type == 'max':
+            x = x.max(dim=1)[0]
+        
+        elif self.pooling_type == 'mean':
+            x = x.mean(dim=1)
+        
+        elif self.pooling_type == 'cls':
+            x = x[:, 0]
+        
+        logits = self.linear_probe(x)
+        return {'logits': logits, 'probabilities': self.softmax(logits), 'predictions': torch.argmax(logits, dim=-1)}
